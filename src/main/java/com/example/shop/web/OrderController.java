08f6cb669dc42fc7cee905b4ed6c3b7f9100dca9
herokuCosmetic <82594303+herokuCosmetic@users.noreply.github.com>
2021-04-15 16:58:57 +0300

    Add files via upload

---
+++ src/main/java/com/example/shop/web/OrderController.java
@@ -0,0 +1,24 @@
+package com.example.shop.web;
+
+
+import com.example.shop.service.OrderService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public OrderController(OrderService orderService, ModelMapper modelMapper) {
+        this.orderService = orderService;
+        this.modelMapper = modelMapper;
+    }
+
+}
